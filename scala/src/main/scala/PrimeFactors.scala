ca1589990b107427cb61195b6936d961d4bc32b9
Byron Delpinal <byronddelpinal@gmail.com>
2016-01-05 15:04:03 -0500

    Scala 8 revised

--- scala/src/main/scala/PrimeFactors.scala
+++ scala/src/main/scala/PrimeFactors.scala
@@ -2,13 +2,25 @@ package com.crebma.primeFactors
 
 object PrimeFactors {
 
-  def factors(num: Int) : List[Int] = {
-    if (num == 1) {
-        List[Int]()
-    } else if (num % 2 == 0 && num > 2) {
-        List[Int](2) ++ factors(num/2)
-    } else {
-        List[Int](num)
+    def isEven(num: Int) : Boolean = {
+        num % 2 == 0
     }
-  }
+
+    def isGreaterThanTwo(num: Int) : Boolean = {
+        num > 2
+    }
+
+    def isOne(num: Int) : Boolean = {
+        num == 1
+    }
+
+    def factors(num: Int, can: Int = 2) : List[Int] = {
+        if (can > num) {
+            List[Int]()
+        } else if (isEven(num) && isGreaterThanTwo(num)) {
+            List[Int](2) ++ factors(num/2)
+        } else {
+            List[Int](num)
+        }
+    }
 }
