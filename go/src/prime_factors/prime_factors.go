0c98ca877970fb472d36633024680c1d233b895a
Byron Delpinal <byronddelpinal@gmail.com>
2016-01-05 16:33:42 -0500

    Finished Go

--- go/src/prime_factors/prime_factors.go
+++ go/src/prime_factors/prime_factors.go
@@ -1,5 +1,15 @@
 package prime_factors
 
 func Factors(num int) []int {
-  return []int{}
+    return getFactors(num, 2);
+}
+
+func getFactors(num int, can int) []int {
+    if can > num {
+        return []int{}
+    } else if (num % can == 0) {
+        return append([]int{can}, getFactors((num / can), can)...)
+    } else {
+        return getFactors(num, can + 1)
+    }
 }
