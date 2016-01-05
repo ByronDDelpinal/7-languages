96162d8fe18db4d0404f1ae085665758312b046a
Byron Delpinal <byronddelpinal@gmail.com>
2016-01-05 10:20:04 -0500

    Clojure fix for all but multiple primes

--- clojure/prime_factors/src/prime_factors/core.clj
+++ clojure/prime_factors/src/prime_factors/core.clj
@@ -1,14 +1,16 @@
 (ns prime-factors.core)
 
 (defn primes
-  ([num]
-    (cond
-        (< num 2)
-            []
-        (and (zero? (rem num 2)) (> num 2))
-            [2, (quot num 2)]
-        :else
-            [num]
+    ([num]
+        (cond
+            (< num 2)
+                []
+            (and (zero? (rem num 2)) (> num 2))
+                (cons 2 (primes (/ num 2)))
+            (and (zero? (rem num 3)))
+                (cons 3 (primes (/ num 3)))
+            :else
+                [num]
         )
     )
 )
