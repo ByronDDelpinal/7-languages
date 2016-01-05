43c518ba327a331fafde307dc4b700287df4be39
Byron Delpinal <byronddelpinal@gmail.com>
2016-01-05 14:22:22 -0500

    Scala 8

--- scala/src/main/scala/PrimeFactors.scala
+++ scala/src/main/scala/PrimeFactors.scala
@@ -3,7 +3,12 @@ package com.crebma.primeFactors
 object PrimeFactors {
 
   def factors(num: Int) : List[Int] = {
-    List[Int]()
+    if (num == 1) {
+        List[Int]()
+    } else if (num % 2 == 0 && num > 2) {
+        List[Int](2) ++ factors(num/2)
+    } else {
+        List[Int](num)
+    }
   }
-
 }
