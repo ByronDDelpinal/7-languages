4ad41b2d4a1441ec6c76973c8bfe702dfcd467c0
Byron Delpinal <byronddelpinal@gmail.com>
2016-01-05 09:58:22 -0500

    Clojure filing test for 8

--- clojure/prime_factors/src/prime_factors/core.clj
+++ clojure/prime_factors/src/prime_factors/core.clj
@@ -1,4 +1,14 @@
 (ns prime-factors.core)
 
 (defn primes
-  ([num] []))
+  ([num]
+    (cond
+        (< num 2)
+            []
+        (and (zero? (rem num 2)) (> num 2))
+            [2, (quot num 2)]
+        :else
+            [num]
+        )
+    )
+)
